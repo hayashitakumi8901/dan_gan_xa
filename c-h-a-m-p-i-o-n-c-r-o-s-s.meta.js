82cae70ea7ee197cdb915cb17215dd44eea6953a
hayashitakumi8901 <hayashitakumi8901@github.com>
2024-06-06 15:34:23 +0700

    up

---
+++ c-h-a-m-p-i-o-n-c-r-o-s-s.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         c-h-a-m-p-i-o-n-c-r-o-s-s
+// @namespace    https://github.com/hayashitakumi8901/dan_gan_xa/raw/main/c-h-a-m-p-i-o-n-c-r-o-s-s.user.js
+// @version      2024.06.06
+// @description  down c-h-a-m-p-i-o-n-c-r-o-s-s
+// @author       You
+// @match        https://championcross.jp/episodes/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=championcross.jp
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/hayashitakumi8901/dan_gan_xa/raw/main/c-h-a-m-p-i-o-n-c-r-o-s-s.user.js
+// @updateURL    https://github.com/hayashitakumi8901/dan_gan_xa/raw/main/c-h-a-m-p-i-o-n-c-r-o-s-s.meta.js
+// ==/UserScript==
